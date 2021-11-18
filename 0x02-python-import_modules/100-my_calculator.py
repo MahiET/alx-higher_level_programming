1e563298102b4606f2331ba38ea3170169314e94
MahiET <meazalemma2020@gmail.com>
2021-11-18 07:05:56 -0800

    My calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
 from sys import argv
+from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
-    
     exit()
 
 argc = len(argv) - 1
