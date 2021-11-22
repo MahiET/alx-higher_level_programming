f4e4cc3b55e4cf9aa02de99f1fcc4b65cb7bee07
MahiET <meazalemma2020@gmail.com>
2021-11-22 05:09:18 -0800

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    blist = [a % 2 == 0 for a in my_list]
+    boolist = [a % 2 == 0 for a in my_list]
     boolist[count] = True
     else:
         boolist[count] = False
