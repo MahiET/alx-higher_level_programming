f50362adf21b4af215059e9e72202faaff711613
MahiET <meazalemma2020@gmail.com>
2021-11-22 00:48:01 -0800

    lsit integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
-
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
     for i in my_list:
-        """
-prints a list of given integers
-"""
-    print("{:d}".format(i))
+        print('{:d}'.format(i))
