48dea8a6dd252b36a39ac169dfe5c63272041073
MahiET <meazalemma2020@gmail.com>
2021-12-10 00:16:09 -0800

    classes square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,9 +9,8 @@ class Square:
     Square class with private instance attribute size
     """
 
-def __init__(self, size=0):
-
-     """
+    def __init__(self, size=0):
+        """
         Args:
             size: size of the square
         """
@@ -21,3 +20,9 @@ def __init__(self, size=0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """
+        Returns area of the square instance
+        """
+        return (self.__size ** 2)
