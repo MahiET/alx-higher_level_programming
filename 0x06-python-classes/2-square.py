c74f529b6cadda7b6dec1ef24448ec9ee98ca4cb
MahiET <meazalemma2020@gmail.com>
2021-12-10 03:57:43 -0800

    paython classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,8 @@ class Square:
     Square class with private instance attribute size
     """
 
-def __init__(self, size=0):
-
-     """
+    def __init__(self, size=0):
+        """
         Args:
             size: size of the square
         """
