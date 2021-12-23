fa7abfc97a34d9844a28d2d0439e30f5fe3c55a1
MahiET <meazalemma2020@gmail.com>
2021-12-23 04:22:15 -0800

    JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This file a function that
+return JSON represrntation
+
+"""
+
+import json
+
+def to_json_string(my_obj):
+
+   """
+return JSON
+
+   """
+   return (json.dumps(my_obj))
