d75987b4b188c38a132181308d2b54dbfbb5b76a
MahiET <meazalemma2020@gmail.com>
2021-12-23 05:02:23 -0800

    Load

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This file contains a function
+that creates an object from a "JSON FILE"
+"""
+import json
+
+def load_from_json_file(filename):
+    """
+     creates object from json file
+    """
+    
+    with open(filename, 'r', encoding="utf-8") as f:
+        return (json.load(f))
+        f.close()
