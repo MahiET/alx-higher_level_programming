ae789b96614bdaa8454742254a53179371770c1c
MahiET <meazalemma2020@gmail.com>
2021-12-23 05:41:02 -0800

    save

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-"""
-This file contains a function that
-writes an obj to a text file using
-JSON rep
-"""
-
 def save_to_json_file(my_obj, filename):
     from json import dumps
     with open(filename, 'w', encoding="utf-8") as f:
