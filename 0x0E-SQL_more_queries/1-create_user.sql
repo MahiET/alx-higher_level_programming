7f78cba64de7168b29673e93e89aed8ad36c0de2
MahiET <meazalemma2020@gmail.com>
2022-02-02 04:20:46 -0800

    Create User

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create an user with password
+-- DDL query to create a MySQL user with pasword
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost
