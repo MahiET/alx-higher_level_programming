55fcd9e0a377eae83430a293ee8c3727b0ce6285
MahiET <meazalemma2020@gmail.com>
2022-02-02 04:18:36 -0800

    PRIVILEGES

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- script that lists all privileges of the MySQL
-SHOW GRANT FOR  user_0d_1@localhost;
-SHOW GRANT FOR  user_0d_2@localhost;
+-- script to run msql commands
+-- DML QUERY to list all privileges for user_0d_1 and user_0d_2 in localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
