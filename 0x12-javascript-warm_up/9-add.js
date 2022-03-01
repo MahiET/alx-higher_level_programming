1cc1fa2abe1a2e28d3a4cdd15c42df4ace966428
MahiET <meazalemma2020@gmail.com>
2022-03-01 06:39:49 -0800

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
 function add (a, b) {
-  return a + b;
+  return (a + b);
 }
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+const result = add(a, b);
+console.log(result);
