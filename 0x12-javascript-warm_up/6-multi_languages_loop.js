549433adcca840e98f7a97c49f0ce610d5ca3ae7
MahiET <meazalemma2020@gmail.com>
2022-03-01 07:00:29 -0800

    Multi Languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let length = arr.length;
-let i = 0;
-
-for (i = 0; i < length; i++) {
-  console.log(arr[i]);
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const string of strings) {
+  console.log(string);
 }
