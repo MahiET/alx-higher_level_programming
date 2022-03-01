1615a296f58f9d70af3060084c7596fb7a6c50c5
MahiET <meazalemma2020@gmail.com>
2022-03-01 03:36:48 -0800

    rectangle one

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print () {
-    let i = 0;
-    for (i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+
+  print (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
+
   rotate () {
-    let w = this.width;
-    let h = this.height;
-    this.height = w;
-    this.width = h;
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
+
   double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
