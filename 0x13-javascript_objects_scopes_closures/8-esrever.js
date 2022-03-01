09a1881b17e59a1c86a54ce886cd4bb3bfedadaa
MahiET <meazalemma2020@gmail.com>
2022-03-01 03:39:02 -0800

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reverse = [];
-  let i = list.length - 1;
-  while (i >= 0) {
+  const reverse = [];
+  for (let i = list.length - 1; i >= 0; i--) {
     reverse.push(list[i]);
-    i--;
   }
   return reverse;
 };
