e4aafd0079e8680a596846c9f835d8e7fc89ffd8
MahiET <meazalemma2020@gmail.com>
2022-03-04 02:14:26 -0800

    My safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,16 +17,4 @@ if __name__ == "__main__":
     for r in lst:
         print(r)
     cur.close()
-    db.close()if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    check = (argv[4], )
-    cur.execute("SELECT * FROM states WHERE name = %s\
-    ORDER BY states.id ASC", check)
-    lst = cur.fetchall()
-    for r in lst:
-        print(r)
-    cur.close()
     db.close()
