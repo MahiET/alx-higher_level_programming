63b7c08c87a679052cd48d993fa61136ee4d41ae
MahiET <meazalemma2020@gmail.com>
2022-03-18 07:34:30 -0700

    sends a POST request to the passed URL with the email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+"""
+
+script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8).
+
+"""
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = {'email': argv[2]}
+    email = urlencode(email)
+    email = email.encode('ascii')
+    request = Request(url, email)
+    with urlopen(request) as response:
+        string = response.read().decode('utf-8')
+        print(string)
