73baec00184c449310c31682abeb3eaced30ffb9
MahiET <meazalemma2020@gmail.com>
2022-03-18 08:54:56 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-from urllib.error import URLError, HTTPError
-from sys import argv
+
 """
 script that takes in url and sends a request to url
 displays body of response
 """
 
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
+
 
 if __name__ == "__main__":
-    rep = urllib.request.Request(argv[1])
+    url = argv[1]
     try:
-        with urllib.request.urlopen(rep) as reply:
-            print(reply.read().decode(encoding="utf-8"))
-    except URLError as err:
-        print("Error code: {}".format(err.code))
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
