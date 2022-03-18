0525dd36f7fd55bfec3b24abff8132358675d194
MahiET <meazalemma2020@gmail.com>
2022-03-18 08:34:27 -0700

    My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username and password) and uses the GitHub API
+"""
+
+from requests import get
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    username = argv[1]
+    password = argv[2]
+    response = get(url, auth=HTTPBasicAuth(username, password))
+    try:
+        obj = response.json()
+        print(obj.get('id'))
+    except ValueError:
+        print(None)
