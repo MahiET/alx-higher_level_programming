1644a230ee11d3f4b6ede197c79884dbfdf430e6
MahiET <meazalemma2020@gmail.com>
2022-03-18 08:50:57 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,6 +3,8 @@
 script to send post request to passed url with email and display
 body as response
 """
+from requests import post
+from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
