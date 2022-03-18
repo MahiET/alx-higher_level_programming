034835005b39d46186f25f0a57a46a6302c73395
MahiET <meazalemma2020@gmail.com>
2022-03-18 09:03:57 -0700

    POST E-mail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 """
-
-script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8).
-
+script that takes in a url and an email, sends a POST request
+to the url with the email as parameter and displays body of the
+response
 """
 
 from urllib.request import urlopen, Request
