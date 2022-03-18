95a84e1d2691d68b25551f65fc3436ff0d84a601
MahiET <meazalemma2020@gmail.com>
2022-03-18 08:29:52 -0700

    JSON formatted and not empty

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+"""
+script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user 
+
+"""
+
+
+if __name__ == "__main__":
+    q = argv[1] if len(argv) > 1 else ""
+    try:
+        re = requests.post('http://0.0.0.0:5000/search_user',
+                           data={'q': q}).json()
+        if 'id' in re and 'name' in re:
+            print("[{}] {}".format(re['id'], re['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
