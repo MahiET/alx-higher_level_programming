08668b828c9de7a5570c4de7b45955edc801f17a
MahiET <meazalemma2020.com>
2022-04-15 14:45:09 +0000

    first argument

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+//The first argument is the file path
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
+fs.readFile(process.argv[2], 'utf8', function (error, data) => {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data);
   }
